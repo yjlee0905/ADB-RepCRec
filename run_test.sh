f9916ab3662560019db7a1d56c747629fa0f91a2
Kyu Doun <KyuDounSim@users.noreply.github.com>
2022-11-30 20:57:30 -0500

    Fixed typo in run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -4,6 +4,6 @@ for FILE in data/*.txt;
 
 do
     echo "Running input $FILE"
-    ../../../usr/bin/java -jar ADB-RepCRec.jar $FILE
-    ../../../usr/bin/java -jar ADB-RepCRec.jar $FILE  > output/outfile_${FILE/\//}
+    ../../../usr/bin/java -jar adb_repcrec.jar $FILE
+    ../../../usr/bin/java -jar adb_repcrec.jar $FILE  > output/outfile_${FILE/\//}
 done
